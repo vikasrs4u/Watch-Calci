e439bb415dd21b356e48ef1037ab3c9cced1690a
Vikas Shetty <vikasrs4u@gmail.com>
2018-08-12 20:01:17 -0700

    Initial Commit

---
+++ Watch Calci/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watch Calci
+//
+//  Created by Vikas R S on 8/12/18.
+//  Copyright © 2018 Vikas Radhakrishna Shetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
