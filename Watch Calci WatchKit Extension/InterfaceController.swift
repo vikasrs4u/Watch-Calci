592f30b865cc4fad1593dac86a020b85008d6360
Vikas Shetty <vikasrs4u@gmail.com>
2018-08-14 18:06:53 -0700

    commit

--- Watch Calci WatchKit Extension/InterfaceController.swift
+++ Watch Calci WatchKit Extension/InterfaceController.swift
@@ -240,7 +240,6 @@ class InterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?)
     {
         super.awake(withContext: context)
-        
         // Configure interface objects here.
         calculationDisplayLabel.setText("\(firstNumber)")
     }
